2c35592a3730b0297762b12c518afdb565956c30
LeaoMartelo <leaomartelo@gmail.com>
2025-01-20 21:26:14 -0300

    push

---
+++ README.md
@@ -0,0 +1,7 @@
+# leao_utils.h
+
+## Collection of small single header utility libraries, mostly in C 
+
+## leao_types.h 
+
+Collection of type definitions and utility functions related to types.
---
+++ leao_types.h
@@ -0,0 +1,57 @@
+#ifndef LEAO_TYPES_H_
+#define LEAO_TYPES_H_
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// vector
+
+typedef struct {
+    float x;
+    float y;
+    float z;
+} vec3_lt;
+
+inline vec3_lt vec3_subtract(vec3_lt a, vec3_lt b) {
+    return (vec3_lt){a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+inline vec3_lt vec3_add(vec3_lt a, vec3_lt b) {
+    return (vec3_lt){a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+inline vec3_lt vec3_multiple(vec3_lt a, vec3_lt b) {
+    return (vec3_lt){a.x * b.x, a.y * b.y, a.z * b.z};
+}
+
+typedef struct {
+    float x;
+    float y;
+} vec2_lt;
+
+inline vec2_lt vec2_subtract(vec2_lt a, vec2_lt b) {
+    return (vec2_lt){a.x - b.x, a.y - b.y};
+}
+
+inline vec2_lt vec2_add(vec2_lt a, vec2_lt b) {
+    return (vec2_lt){a.x + b.x, a.y + b.y};
+}
+
+inline vec2_lt vec2_multiple(vec2_lt a, vec2_lt b) {
+    return (vec2_lt){a.x * b.x, a.y * b.y};
+}
+
+// c string utilities
+
+static inline const char *bool_to_cstr(int value) {
+    return value ? "true" : "false";
+}
+
+static inline char *int_to_cstr(int value, char *buffer) {
+
+    snprintf(buffer, sizeof(buffer), "%d", value);
+    return buffer;
+}
+
+#endif // LEAO_TYPES_H_
